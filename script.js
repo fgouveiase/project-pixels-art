966246479ca203e7a9c6f9c129c17dcc69105b59
Felipe Gouveia <felipegouveia.bfr@hotmail.com>
2022-08-08 17:37:45 -0300

    tentando resolver questao 8 again

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
-const pixels = document.querySelectorAll('.pixel');
 const cores = document.querySelectorAll('.color');
 const pixelBoard = document.getElementById('#pixel-board');
 
 function removeSelect() {
-  for (i = 0; i < cores.length; i += 1) {
+  for (let i = 0; i < cores.length; i += 1) {
     cores[i].classList.remove('selected');
   }
 }
 
-function addSelect(e) {
-  e.target.classList.add('selected');
-}
-
-for (i = 0; i < cores.length; i += 1) {
-  cores[i].addEventListener('click', removeSelect);
-  cores[i].addEventListener('click', addSelect);
-}
-
-function aplicaCor(event) {
-  if (event.target.classList.contains.pixels) {
-    const cor = document.querySelector('.selected').style.backgroundColor;
-    event.target.style.backgroundColor = cor;
+function addSelect() {
+  for (let i = 0; i < cores.length; i += 1) {
+    cores[i].classList.add('selected');
   }
 }
 
-window.onload = function () {
-  for (i = 0; i < cores.length; i += 1) {
+function addEvento() {
+  for (let i = 0; i < cores.length; i += 1) {
+    cores[i].addEventListener('click', removeSelect);
     cores[i].addEventListener('click', addSelect);
-    pixelBoard.addEventListener('click', aplicaCor);
   }
+}
+
+function aplicaCor(e) {
+  if (e.target.classList === 'pixel') {
+    const cor = document.querySelector('.selected').style.backgroundColor;
+    e.target.style.backgroundColor = cor;
+  }
+}
+
+window.onload = function janela() {
+  addEvento();
+  pixelBoard.addEventListener('click', aplicaCor);
 };
