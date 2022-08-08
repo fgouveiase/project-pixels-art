8752e1d437d06069d078494fe2ec3dc0f3518a96
Felipe Gouveia <felipegouveia.bfr@hotmail.com>
2022-08-08 16:43:24 -0300

    resolvendo questao 8

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+const pixels = document.querySelectorAll('.pixel');
 const cores = document.querySelectorAll('.color');
+const pixelBoard = document.getElementById('#pixel-board');
 
 function removeSelect() {
   for (i = 0; i < cores.length; i += 1) {
@@ -14,3 +16,14 @@ for (i = 0; i < cores.length; i += 1) {
   cores[i].addEventListener('click', removeSelect);
   cores[i].addEventListener('click', addSelect);
 }
+
+function corSelecionada(event) {
+  const corSelecionada = document.querySelector('.selected');
+  event.target.style.backgroundColor = corSelecionada.style.backgroundColor;
+}
+
+function aplicaCor() {
+  pixelBoard.addEventListener('click', corSelecionada);
+}
+
+aplicaCor();
